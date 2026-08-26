35edefa2aef3ce79e64d4d2df0b528e1f37d8f77
Emilio Pinzon <epinzon-2026037@kinal.edu.gt>
2026-08-26 11:48:57 -0600

    Correccion de errores

--- src/org/esperanza/Dao/Dao.java
+++ src/org/esperanza/Dao/Dao.java
@@ -1,18 +1,6 @@
 
 package org.esperanza.Dao;
 
-import modelo.Libro;
-import java.util.List;
-
 public interface Dao {
 
-    boolean insertar(Libro libro);
-
-    boolean actualizar(Libro libro);
-
-    boolean eliminar(int id);
-
-    Libro buscarPorId(int id);
-
-    List<Libro> listarTodos();
 }
