4c22ed2aee27d45d0d690cd876615b808adc824a
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-09-11 08:31:45 -0600

    feat(model): Actualizar clase modelo Libro con stock y estado

--- src/org/esperanza/model/Libro.java
+++ src/org/esperanza/model/Libro.java
@@ -1,9 +1,11 @@
 package org.esperanza.model;
 
+import java.sql.Date;
+
 public class Libro {
     private String isbn;
     private String titulo;
-    private String fechaPublicacion;
+    private Date fechaPublicacion;
     private double precio;
     private int idCategoria;
     private String nitEditorial;
@@ -11,18 +13,32 @@ public class Libro {
     private int stockActual;
     private int stockMinimo;
     private boolean activo;
-    private String nombreAutor;
 
     public Libro() {}
 
+    public Libro(String isbn, String titulo, Date fechaPublicacion, double precio, int idCategoria, 
+                 String nitEditorial, int idProveedor, int stockActual, int stockMinimo, boolean activo) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.fechaPublicacion = fechaPublicacion;
+        this.precio = precio;
+        this.idCategoria = idCategoria;
+        this.nitEditorial = nitEditorial;
+        this.idProveedor = idProveedor;
+        this.stockActual = stockActual;
+        this.stockMinimo = stockMinimo;
+        this.activo = activo;
+    }
+
+ 
     public String getIsbn() { return isbn; }
     public void setIsbn(String isbn) { this.isbn = isbn; }
 
     public String getTitulo() { return titulo; }
     public void setTitulo(String titulo) { this.titulo = titulo; }
 
-    public String getFechaPublicacion() { return fechaPublicacion; }
-    public void setFechaPublicacion(String fechaPublicacion) { this.fechaPublicacion = fechaPublicacion; }
+    public Date getFechaPublicacion() { return fechaPublicacion; }
+    public void setFechaPublicacion(Date fechaPublicacion) { this.fechaPublicacion = fechaPublicacion; }
 
     public double getPrecio() { return precio; }
     public void setPrecio(double precio) { this.precio = precio; }
@@ -44,7 +60,4 @@ public class Libro {
 
     public boolean isActivo() { return activo; }
     public void setActivo(boolean activo) { this.activo = activo; }
-
-    public String getNombreAutor() { return nombreAutor; }
-    public void setNombreAutor(String nombreAutor) { this.nombreAutor = nombreAutor; }
 }
