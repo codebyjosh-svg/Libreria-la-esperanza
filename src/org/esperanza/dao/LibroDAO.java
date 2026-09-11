af16cbb6e6fe31f283871792f287ccfdb4019b42
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-09-11 08:46:28 -0600

    feat(dao): Actualizar LibroDAO e implementar consultas por ISBN

--- src/org/esperanza/dao/LibroDAO.java
+++ src/org/esperanza/dao/LibroDAO.java
@@ -1,19 +1,15 @@
 package org.esperanza.dao;
 
-import org.esperanza.model.Libro; 
 import java.util.List;
+import org.esperanza.model.Libro;
 
 public interface LibroDAO {
-
-    Libro buscarPorIsbn(String isbn);
-
-    List<Libro> buscarPorTitulo(String titulo);
-
-    List<Libro> buscarPorAutor(String autor);
-
     List<Libro> listarTodos();
+    Libro buscarLibro(String isbn);
+    boolean insertar(Libro libro);
+    boolean actualizar(Libro libro);
+    boolean eliminar(String isbn); 
 }
-
 
 
 
