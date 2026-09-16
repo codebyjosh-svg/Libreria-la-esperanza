d9432e718616a625ae65d2c45c42d889e849a92c
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-09-16 11:23:11 -0600

    feat(bodega): [T3.5.7] Probar navegacion
    Se valido la integracion completa de la navegacion por roles, la carga del dashboard de bodega y el funcionamiento correcto de la vista y eventos.

--- src/org/esperanza/Controller/DashboardBodegaController.java
+++ src/org/esperanza/Controller/DashboardBodegaController.java
@@ -3,7 +3,6 @@ package org.esperanza.Controller;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
